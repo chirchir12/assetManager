62bb46e956bb3570dbd653f9ae076048df16ba16
chirchir <chirchir7370@gmail.com>
2020-05-26 14:07:25 +0300

    updated reqirement.file

--- config/settings.py
+++ config/settings.py
@@ -83,7 +83,7 @@ AUTH_USER_MODEL = 'users.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
